3117b649b5cdc45f2b3bf2bcad3aa5fb964a4b54
emontilla07 <emontilla107@gmail.com>
2022-11-08 16:38:16 -0500

    Add content on HTML

--- lab-8/assets/js/app.js
+++ lab-8/assets/js/app.js
@@ -1,31 +1,35 @@
-const form = document.querySelector('form');
-const text = document.querySelector('#text-value');
-const result = document.querySelector('#result');
-const btnClear = document.querySelector('#btn-clear');
-
-form.addEventListener('submit', (e) => {
-    calc();
-    e.preventDefault();
-});
-
-const calc = () => {
+(() => {
+    const form = document.querySelector('#form');
+    const text = document.querySelector('#text-value');
+    const result = document.querySelector('#result');
+    const btnClear = document.querySelector('#btn-clear');
     const count = {};
-    const text2 = text.value;
-    const newText = text2.split("");
-    for (const element in newText) {
-        count[newText[element]] = (newText[element] in count) ? count[newText[element]] + 1 
-                                                              : count[newText[element]] = 1;
+
+    form.addEventListener('submit', (e) => {
+        calc();
+        e.preventDefault();
+    });
+
+    const calc = () => {
+        const text2 = text.value;
+        const newText = text2.split("");
+        for (const element in newText) {
+            count[newText[element]] = (newText[element] in count) ? count[newText[element]] + 1 
+                                                                  : count[newText[element]] = 1;
+        }
+        const key = Object.keys(count);
+        const value = Object.values(count);
+        for (let i = 0; i < key.length; i++) {
+            result.innerHTML += `<p class="format-text">${key[i]} = ${value[i]}</p>`;
+        }
+        btnClear.style.visibility = 'visible';
     }
-    result.innerHTML = `<p class="format-text">${Object.keys(count)}</p>`;
-    console.log({count});
-    btnClear.style.visibility = 'visible';
-}
 
-const clear = () => {
-    form.reset();
-    result.innerHTML = '';
-    text.innerHTML = '';
-    btnClear.style.visibility = 'hidden';
-}
+    const clear = () => {
+        location.reload();
+        result.innerHTML = '';
+        btnClear.style.visibility = 'hidden';
+    }
 
-btnClear.addEventListener('click', clear);
+    btnClear.addEventListener('click', clear);
+})();
